a7224867d4a9634decf91bcb12f11361c235cb7b
José <32659580+josemqz@users.noreply.github.com>
2020-01-12 19:33:03 -0300

    tecnicoFiles

---
+++ www/www/js/consultas.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+
+
+    //variables del chat
+    let chat = $("#chat");
+
+    //variable del nav
+    let nav = $("#nav");
+    let home = $("#HomeButton");
+    let qCreate = $("#MakeQuery");
+
+    //variabes de consultas
+    let query = $("#queries");
+    let title = $("#title");
+    let descrip = $("#descrip");
+
+    let consultas = $("#consultas");
+
+    var ultimoMensaje = 0;
+
+
+    var usrId = parseInt(window.localStorage.getItem("token"),10);
+
+    let settingsGetConsultas = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:8000/consulta/getAll",
+        "method": "GET",
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "processData": true,
+        "data": ""
+    }
+
+
+    //////////////////// get consultas
+    $.ajax(settingsGetConsultas).done(function(respuesta){
+            respuesta.forEach(element => {
+                if(element["usuarioId"] === usrId){
+                    consultas.append(' <br>                                                    \
+                    <div class="card">                                                       \
+                        <div class="card-body">                                              \
+                            <h5 class="card-title">Consulta '+ element["titulo"] + '</h5>         \
+                            <p class="card-text">Cliente: '+ element["usuarioId"] + '</p>    \
+                            <p class="card-text">Técnico: '+ element["tecnicoId"] + '</p>    \
+                            <a href="/messages.html" onclick="setConsulta('+ element["consultaId"] + ')"\
+                            class="btn btn-primary">Ir al Chat</a>                           \
+                        </div>                                                               \
+                    </div>                                                                   \
+                ');
+                }           
+            });
+    });
+
+    /////////////////////End get consultas
+
+    home.click(function () {
+        window.location.href = "/menu.html";
+    });
+
+    chat.click(function () {
+        window.location.href = "/messages.html";
+    });
+
+    qCreate.click(function () {
+        window.location.href = "/menu.html";
+    });
+});
+
+function setConsulta(idConsulta) {
+    window.localStorage.setItem('consulta', idConsulta.toString());
+}
---
+++ www/www/js/menu.js
@@ -0,0 +1,89 @@
+$(document).ready(function () {
+
+    //variables del chat
+    let chat = $("#MessagesButton");
+
+    //variable del nav
+    let nav = $("#nav");
+    let home = $("#HomeButton");
+    let qCreate = $("#MakeQuery");
+
+    //variabes de consultas
+    let query = $("#queries");
+    let title = $("#title");
+    let descrip = $("#descrip");
+
+
+    // settings de postman para get, con esto funciona sin problemas
+    let settingsGETall = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:8000/cliente/getAll",
+        "method": "GET",
+        "headers": {
+            'Access-Control-Allow-Origin':'*',
+            "Content-Type": "application/json",
+        },
+        "processData": true,
+        "data": ""
+    }
+
+    let usrId = "";
+    let getID = function () {
+        $.ajax(settingsGETall).done(function (response) {
+            // this is pretty hardcore lmao; 
+            //se trunca la tabal del json en id, porque este no se necesita
+            response.forEach(element => {
+                if (window.localStorage.getItem("token") === ""){
+                    if ((element["nombre"] === window.localStorage.getItem("name")) && 
+                    (element["pass"] === window.localStorage.getItem("pass"))) {
+                        usrId = parseInt(element["id"], 10);
+                        window.localStorage.setItem("token", usrId);
+                    }
+                }
+            });
+        })
+    }
+    getID();
+
+    query.submit(function (e) {//yay
+        e.preventDefault();
+        if ((title.val() !== "") && (descrip.val() !== "")) {
+            // https://noodle-project-anal.herokuapp.com
+            let date = new Date();
+            usrId = parseInt(window.localStorage.getItem("token"), 10);
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8000/consulta/consulta",// api url
+                data: JSON.stringify({
+                    titulo: title.val(), descripcion: descrip.val(),
+                    fecha: date.toDateString(), 
+                    usuarioId: usrId,
+                }),
+                dataType: 'json',
+                contentType: "application/json",
+                success: function (data) {
+                    console.log("xd");
+                },
+                failure: function (errMsg) { console.log("lmao") }
+            });
+            nav.append("<h6 style='color: black; font - size: 2.5em; text-align: center'><br>Consulta Realizada</h6>");
+            query.fadeOut();
+        }
+    });
+
+   home.click(function () {
+       $("#form").fadeOut();
+    });
+
+    chat.click(function () {
+        window.location.href = "/consultas.html";
+    });
+
+    qCreate.click(function () {
+        query.fadeIn();
+    });
+
+    
+    nav.hide();
+})
